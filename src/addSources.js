e1620fc4302bca0fc60a60df93b3c03a65b52fae
tjmsy <18753632+tjmsy@users.noreply.github.com>
2026-05-08 20:44:59 +0900

    feat: allow reusing existing sources across isomizer projects

--- src/addSources.js
+++ src/addSources.js
@@ -1,5 +1,9 @@
 export async function addSources(map, sourcesJson) {
   Object.entries(sourcesJson).forEach(([sourceId, sourceData]) => {
+    if (map.getSource(sourceId)) {
+      return;
+    }
+
     map.addSource(sourceId, sourceData);
   });
 }
